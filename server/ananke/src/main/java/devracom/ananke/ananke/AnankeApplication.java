98cf4c164f3b2d47cb959f0500c6e97c2c1e10f8
Ramphy Aquino Nova <30904905+Ramphy0x90@users.noreply.github.com>
2022-11-07 15:44:54 +0100

    Back-end structure init

---
+++ server/ananke/src/main/java/devracom/ananke/ananke/AnankeApplication.java
@@ -0,0 +1,13 @@
+package devracom.ananke.ananke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnankeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnankeApplication.class, args);
+	}
+
+}
---
+++ server/ananke/src/test/java/devracom/ananke/ananke/AnankeApplicationTests.java
@@ -0,0 +1,13 @@
+package devracom.ananke.ananke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnankeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
