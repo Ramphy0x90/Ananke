1320f40aeb498dbc87499bc40d4245e2559d706f
ramphy0x90 <ramphy11@hotmail.com>
2022-11-28 18:15:18 +0100

    Ticket models & submodels

---
+++ src/main/java/devracom/ananke/ananke/Ticket/models/Priority.java
@@ -0,0 +1,15 @@
+package devracom.ananke.ananke.Ticket.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Priority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private Long weight;
+}
