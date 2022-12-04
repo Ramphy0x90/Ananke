ca4d0fd1929e6327c8e4662166f5da947104025a
ramphy0x90 <ramphy11@hotmail.com>
2022-12-04 18:14:27 +0100

    Default ticket priorities

--- src/main/java/devracom/ananke/ananke/config/DataConfig.java
+++ src/main/java/devracom/ananke/ananke/config/DataConfig.java
@@ -2,6 +2,8 @@ package devracom.ananke.ananke.config;
 
 import devracom.ananke.ananke.Role.Role;
 import devracom.ananke.ananke.Role.RoleRepository;
+import devracom.ananke.ananke.Ticket.models.Priority;
+import devracom.ananke.ananke.Ticket.repositories.PriorityRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,14 +14,29 @@ import java.util.List;
 @Configuration
 public class DataConfig {
     @Bean
-    CommandLineRunner commandLineRunner(RoleRepository roleRepository) throws IOException {
+    CommandLineRunner commandLineRunner(RoleRepository roleRepository,
+                                        PriorityRepository priorityRepository) throws IOException {
         return args -> {
+            // Default roles reference string
             List<String> defaultRoles = List.of(
                     "ROLE_ADMIN", "ROLE_USER", "ROLE_SUPERVISOR", "ROLE_DIVISION_HEAD", "ROLE_EXTERNAL"
             );
 
+            // Default tickets priorities reference string
+            List<String> defaultPriorities = List.of(
+                    "High", "Medium", "Low"
+            );
+
+            // Create default roles
             for(String role: defaultRoles) {
                 roleRepository.save(new Role(role));
+            }
+
+            // Create default priorities
+            for(int i = 0; i < defaultPriorities.size(); i++) {
+                priorityRepository.save(
+                        new Priority(defaultPriorities.get(i), defaultPriorities.size() - i)
+                );
             }
         };
     }
